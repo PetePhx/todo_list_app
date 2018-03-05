e60c18a316e773b28a5fae3e29bd2b5883bb9e2a
PetePhx <pete0phx@yandex.com>
2018-03-04 22:35:09 -0700

    fix minor bug for jquery parent element

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,7 +7,6 @@ $(function() {
     var ok = confirm("Are you sure? This cannot be undone!");
     if (ok) {
       var form = $(this);
-
       var request = $.ajax({
         url: form.attr("action"),
         method: form.attr("method")
@@ -15,7 +14,7 @@ $(function() {
 
       request.done(function(data, textStatus, jqXHR) {
         if (jqXHR.status === 204) {
-          form.parent("li").remove();
+          form.parents('li').remove();
         } else if (jqXHR.status === 200) {
           document.location = data;
         }
